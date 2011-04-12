911a4b70f61e90c47cfc959f272d7edf1feb4bc5
Boussard <y.boussard@free.fr>
2011-04-12 16:53:12 +0200

    prepare release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1.1'
+version = '0.2.0'
 
 setup(name='collective.blueprint.jsonmigrator',
       version=version,
@@ -13,8 +13,8 @@ setup(name='collective.blueprint.jsonmigrator',
         "Programming Language :: Python",
         ],
       keywords='',
-      author='',
-      author_email='',
+      author='garbas + yboussard',
+      author_email='y.boussard@alterway.fr',
       url='http://svn.plone.org/svn/collective/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
